a638cd2cc44b8a73fd96caea6f21df9777932762
= <=>
2021-01-27 15:52:19 -0300

    add tags

--- dist/assets/js/wf-plugin.js
+++ dist/assets/js/wf-plugin.js
@@ -1,1 +1,93 @@
-class Helper{getUrlParameter(e){const t=top.location.search.substring(1).split("&");for(let i=0;i<t.length;i++){let o=t[i].split("=");if(o[0]===e)return o[1]}}getUrlWord(e){return new RegExp("\\b"+e+"\\b","i").test(window.location.href)}offset(e){let t=e.getBoundingClientRect();const i=window.pageXOffset||document.documentElement.scrollLeft,o=window.pageYOffset||document.documentElement.scrollTop;return{top:t.top+o,left:t.left+i}}verifyUrlRoute(e){return window.location.pathname.split("/").indexOf(e)>-1}wrapItem(e,t){const i=document.createElement("div");i.className=t,e.parentNode.insertBefore(i,e),i.appendChild(e)}}export{Helper};class Layout{constructor(){this.breakPointExtraSmall=0,this.breakPointSmall=576,this.breakPointMedium=768,this.breakPointBig=992,this.breakPointExtraBig=1200,this.breakPointFullHd=1920}}export{Layout};class LoadingMain{constructor(){this.cssHide="hide",this.cssAnimation="animate",this.elWrapper=document.querySelector(".loading-main"),this.elLoading=this.elWrapper.querySelector(".loading"),this.elBody=document.querySelector("body")}hide(){this.elWrapper.classList.add(this.cssHide),this.elLoading.classList.remove(this.cssAnimation),this.elBody.style.overflow="auto"}}export{LoadingMain};window.loadingMain=new LoadingMain,window.addEventListener("load",window.loadingMain.hide(),{once:!0});
+class Helper {
+    getUrlParameter(target) {
+        const url = top.location.search.substring(1);
+        const parameter = url.split('&');
+
+        for (let i = 0; i < parameter.length; i++) {
+            let parameterName = parameter[i].split('=');
+
+            if (parameterName[0] === target) {
+                return parameterName[1];
+            }
+        }
+    }
+
+    getUrlWord(target) {
+        return new RegExp('\\b' + target + '\\b', 'i').test(window.location.href);
+    }
+
+    offset(element) {
+        let rect = element.getBoundingClientRect();
+        const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        const obj = {
+            'top': rect.top + scrollTop,
+            'left': rect.left + scrollLeft,
+        };
+
+        return obj;
+    }
+
+    verifyUrlRoute(target) {
+        const arrFolder = window.location.pathname.split('/');
+
+        if (arrFolder.indexOf(target) > -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    wrapItem(target, cssClass) {
+        const wrapper = document.createElement('div');
+
+        wrapper.className = cssClass;
+        target.parentNode.insertBefore(wrapper, target);
+        wrapper.appendChild(target);
+    }
+}
+
+export {
+    Helper
+};
+class Layout {
+    constructor() {
+        this.breakPointExtraSmall = 0;
+        this.breakPointSmall = 576;
+        this.breakPointMedium = 768;
+        this.breakPointBig = 992;
+        this.breakPointExtraBig = 1200;
+        this.breakPointFullHd = 1920;
+    }
+}
+
+export {
+    Layout
+};
+class LoadingMain {
+    constructor() {
+        this.cssHide = 'hide';
+        this.cssAnimation = 'animate';
+
+        this.elWrapper = document.querySelector('.loading-main');
+        this.elLoading = this.elWrapper.querySelector('.loading');
+        this.elBody = document.querySelector('body');
+    }
+
+    hide() {
+        this.elWrapper.classList.add(this.cssHide);
+        this.elLoading.classList.remove(this.cssAnimation);
+        this.elBody.style.overflow = 'auto';
+    }
+}
+
+export {
+    LoadingMain
+};
+window.loadingMain = new LoadingMain();
+
+window.addEventListener('load',
+    window.loadingMain.hide(),
+     {
+        once: true
+    });
